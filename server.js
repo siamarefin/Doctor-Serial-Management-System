9bb63ef2035013ea80b415eb350c57add3ec5b40
Siam arefin <siam12@student.sust.edu>
2024-06-05 10:25:40 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express  = require('express') 
+const colors = require('colors')
+const moragan = require('morgan')
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+const app = express() 
+
+app.use(express.json())
+app.use(moragan('dev'))
+
+
+app.get('/', (req , res )=> {
+    res.status(200).send({
+        Message:"server running",
+    });
+});
+
+
+
+const port  = process.env.PORT || 8080
+
+app.listen(port  ,()  => {
+    console.log(
+        `Server Running  in  ${process.env.NODE_MODE} `
+    );
+});
