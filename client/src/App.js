962826bc73b150b8b70151b53c983544ec7ccebd
Siam Arefin <siam12@student.sust.edu>
2024-06-11 23:39:05 +0600

    wedfg

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import login from './pages/login';
+import Register from './pages/Register';
 function App() {
   return (
     <> 
-      <div className = "bg-dark" >
-        <h1 className = "text-success"> Hello </h1> 
-      </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path = '/' element  = {<Homepage/>} />
+        <Route path = '/login' element = {<login/> }/> 
+        <Route path ='/register' element  = {< Register/>} /> 
+
+      </Routes>
+      
+      
+      
+      
+      
+      </BrowserRouter>
+
+
 
     </> 
   );
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Homepage = () => {
   return (
     <div>
+      <h1> Home page </h1> 
       
     </div>
   )
