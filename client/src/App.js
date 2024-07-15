2d9eca72620e8abc6bbb68dcbe3cd48c9ff528ee
Siam Arefin <siam12@student.sust.edu>
2024-07-15 07:47:38 +0600

    run

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import Login from './pages/Login';
+import Login from './pages/login';
 import Register from './pages/Register';
 function App() {
   return (
